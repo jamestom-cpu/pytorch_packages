f68eac6223715e38e7bbe94ff16a0162ba2d5389
tomas <tomas@DESKTOP-Q2QRKD3.localdomain>
2022-09-05 00:19:03 +0200

    added imports

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,31 @@
+import os
+import random
+
+import torch
+import torchvision
+import tarfile
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import torch.nn.functional as F
+from torchvision.datasets.utils import download_url
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
+
+from tqdm.notebook import tqdm
+from pprint import pprint
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+matplotlib.rcParams['figure.facecolor'] = '#ffffff'
+
+
+
+
 ## utils functions
 
 ## Use GPU
